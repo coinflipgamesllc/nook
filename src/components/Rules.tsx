d8854b3e320ae8a896d72287f8296a09c9581f1b
Robert Newton <robert.newton11@gmail.com>
2022-03-05 13:49:18 -0800

    update with new art

--- src/components/Rules.tsx
+++ src/components/Rules.tsx
@@ -1,110 +1,116 @@
-import React, { Dispatch, FC, useState, Fragment } from 'react';
-import { Actions, Action } from '../types/GameState';
+import React, { Dispatch, FC, useState, Fragment } from "react";
+import { Actions, Action } from "../types/GameState";
 
-import './Rules.scss';
+import "./Rules.scss";
 
-import title from '../img/title.png';
-import matching from '../img/rules-matching.png';
-import discards from '../img/rules-discards.png';
-import perfect from '../img/rules-perfect-match.png';
+import title from "../img/title.png";
+import matching from "../img/rules-matching.png";
+import discards from "../img/rules-discards.png";
+import perfect from "../img/rules-perfect-match.png";
 
 interface RulesProps {
-    dispatch: Dispatch<Action>;
-    resume: boolean;
+  dispatch: Dispatch<Action>;
+  resume: boolean;
 }
 
 const Rules: FC<RulesProps> = ({ dispatch, resume }) => {
-    const [page, setPage] = useState(0);
+  const [page, setPage] = useState(0);
 
-    let instructions = <Fragment />
-    switch (page) {
-        case 0:
-        instructions = (
-            <Fragment>
-                <p>
-                    Compete with your fellow interior designers to suggest ideas for how this nook should look.
-                </p>
-                <img src={title} alt="Nook title" />
-            </Fragment>
-        );
-        break;
+  let instructions = <Fragment />;
+  switch (page) {
+    case 0:
+      instructions = (
+        <Fragment>
+          <p>
+            Compete with your fellow interior designers to suggest ideas for how
+            this nook should look.
+          </p>
+          <img src={title} alt="Nook title" />
+        </Fragment>
+      );
+      break;
 
-        case 1:
-        instructions = (
-            <Fragment>
-                <p>
-                    Play cards from your hand to create matches with the card in the center.
-                </p>
-                <img src={matching} alt="Rules on matching" />
-            </Fragment>
-        );
-        break;
+    case 1:
+      instructions = (
+        <Fragment>
+          <p>
+            Play cards from your hand to create matches with the card in the
+            center.
+          </p>
+          <img src={matching} alt="Rules on matching" />
+        </Fragment>
+      );
+      break;
 
-        case 2:
-        instructions = (
-            <Fragment>
-                <p>
-                    The better the match, the more cards you can discard. Discard your hand first to win the round.
-                </p>
-                <img src={discards} alt="Rules on discarding" />
-            </Fragment>
-        );
-        break;
+    case 2:
+      instructions = (
+        <Fragment>
+          <p>
+            The better the match, the more cards you can discard. Discard your
+            hand first to win the round.
+          </p>
+          <img src={discards} alt="Rules on discarding" />
+        </Fragment>
+      );
+      break;
 
-        case 3:
-        instructions = (
-            <Fragment>
-                <p>
-                    You can also play a perfect match at any time &mdash; even when it's not your turn.
-                </p>
-                <img src={perfect} alt="Rules on perfect matches" />
-            </Fragment>
-        );
-        break;
+    case 3:
+      instructions = (
+        <Fragment>
+          <p>
+            You can also play a perfect match at any time &mdash; even when it's
+            not your turn. If you do, it'll become your turn immediately!
+          </p>
+          <img src={perfect} alt="Rules on perfect matches" />
+        </Fragment>
+      );
+      break;
 
-        case 4:
-        instructions = (
-            <Fragment>
-                <p>
-                    Be the first designer to empty your hand and you'll win the round! Win 3 rounds and you'll win the game. Good Luck!
-                </p>
-            </Fragment>
-        );
-        break;
-    }
+    case 4:
+      instructions = (
+        <Fragment>
+          <p>
+            Be the first designer to empty your hand and you'll win the round!
+            Win 3 rounds and you'll win the game. Good Luck!
+          </p>
+        </Fragment>
+      );
+      break;
+  }
 
-    return (
-        <div className="rules">
-            <h1>nook</h1>
+  return (
+    <div className="rules">
+      <h1>nook</h1>
 
-            <div className="instructions-box">
-                {instructions}
-                
-                <div className="instruction-page-buttons">
-                    <button 
-                        onClick={() => setPage(page - 1)}
-                        disabled={page === 0}
-                    >&larr;</button>
+      <div className="instructions-box">
+        {instructions}
 
-                    <button 
-                        onClick={() => setPage(page + 1)}
-                        disabled={page === 4}
-                    >&rarr;</button>
-                </div>
-            </div>
+        <div className="instruction-page-buttons">
+          <button onClick={() => setPage(page - 1)} disabled={page === 0}>
+            &larr;
+          </button>
 
-            <button 
-                className={page === 4 ? 'highlight' : ''}
-                onClick={() => {
-                    resume
-                        ? dispatch({ type: Actions.ResumeGame, payload: null })
-                        : dispatch({ type: Actions.StartGame, payload: { numHumans: 1, numBots: 3 }});
-                }}
-            >
-                {resume ? 'Resume' : 'Start'} Game
-            </button>
+          <button onClick={() => setPage(page + 1)} disabled={page === 4}>
+            &rarr;
+          </button>
         </div>
-    )
-}
+      </div>
 
-export default Rules
+      <button
+        className={page === 4 ? "highlight" : ""}
+        onClick={() => {
+          resume
+            ? dispatch({ type: Actions.ResumeGame, payload: null })
+            : dispatch({
+                type: Actions.StartGame,
+                payload: { numHumans: 1, numBots: 3 },
+              });
+        }}
+      >
+        {resume ? "Resume" : "Start"} Game
+      </button>
+    </div>
+  );
+};
+
+export default Rules;
--- src/types/Deck.ts
+++ src/types/Deck.ts
@@ -1,48 +1,45 @@
-import { Card } from './Card';
-import { Location } from './Location';
+import { Card } from "./Card";
+import { Location } from "./Location";
 
-export class Deck extends Array<Card>
-{
-    static create(): Deck
-    {
-        let deck = new Deck();
+export class Deck extends Array<Card> {
+  static create(): Deck {
+    let deck = new Deck();
 
-        const colors = ['blue', 'grey', 'red'];
-        const wallpapers = ['flower', 'stripes', 'chevron'];
-        const decorations = ['clock', 'picture', 'plant'];
+    const colors = ["blue", "purple", "red"];
+    const wallpapers = ["flowers", "stripes", "circles"];
+    const decorations = ["clock", "painting", "plant"];
 
-        for (let i = 0; i < 2; i++) {
-            colors.forEach((color) => {
-                wallpapers.forEach((wallpaper) => {
-                    decorations.forEach((decoration) => {
-                        deck.push({
-                            id: deck.length,
-                            color,
-                            wallpaper,
-                            decoration,
-                            location: Location.Deck,
-                            visible: false,
-                            pos: { x: 0, y: 0 },
-                            owner: null,
-                            animating: false,
-                        });
-                    });
-                });
+    for (let i = 0; i < 2; i++) {
+      colors.forEach((color) => {
+        wallpapers.forEach((wallpaper) => {
+          decorations.forEach((decoration) => {
+            deck.push({
+              id: deck.length,
+              color,
+              wallpaper,
+              decoration,
+              location: Location.Deck,
+              visible: false,
+              pos: { x: 0, y: 0 },
+              owner: null,
+              animating: false,
             });
-        }
-
-        return deck.shuffle();
+          });
+        });
+      });
     }
 
-    shuffle(): Deck
-    {
-        let d = new Deck();
-        d.push(
-            ...this.map((value) => ({ value, sort: Math.random() }))
-                .sort((a, b) => a.sort - b.sort)
-                .map(({ value }) => value)
-        );
+    return deck.shuffle();
+  }
 
-        return d;
-    }
-};
+  shuffle(): Deck {
+    let d = new Deck();
+    d.push(
+      ...this.map((value) => ({ value, sort: Math.random() }))
+        .sort((a, b) => a.sort - b.sort)
+        .map(({ value }) => value)
+    );
+
+    return d;
+  }
+}
