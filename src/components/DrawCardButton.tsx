886b721cc929016a999f147473a26e1129779ce4
Robert Newton <robert.newton11@gmail.com>
2022-03-11 20:23:06 -0800

    move and layer elements for small screens

--- src/components/DrawCardButton.tsx
+++ src/components/DrawCardButton.tsx
@@ -1,23 +1,31 @@
-import React, { FC, Dispatch, Fragment, CSSProperties } from 'react';
-import { GameState, Action, Actions } from '../types/GameState';
-import { Position } from '../types/Position';
-import { Phase } from '../types/Turn';
+import React, { FC, Dispatch, Fragment, CSSProperties } from "react";
+import { GameState, Action, Actions } from "../types/GameState";
+import { Position } from "../types/Position";
+import { Phase } from "../types/Turn";
 
 type DrawCardButtonProps = GameState & {
-    dispatch: Dispatch<Action>;
-    pos: Position;
-}
+  dispatch: Dispatch<Action>;
+  pos: Position;
+};
 
 const DrawCardButton: FC<DrawCardButtonProps> = ({ turn, dispatch, pos }) => {
-    const style: CSSProperties = {
-        position: 'relative',
-        top: `${pos.y + 210}px`,
-        left: `${pos.x}px`,
-    }
+  const style: CSSProperties = {
+    position: "relative",
+    top: `${pos.y + 110}px`,
+    left: `${pos.x}px`,
+    zIndex: 999,
+  };
 
-    return turn.player === 0 && turn.phase === Phase.Play
-        ? <button style={style} onClick={() => dispatch({ type: Actions.Draw, payload: { player: 0 }})}>Draw</button>
-        : <Fragment />
+  return turn.player === 0 && turn.phase === Phase.Play ? (
+    <button
+      style={style}
+      onClick={() => dispatch({ type: Actions.Draw, payload: { player: 0 } })}
+    >
+      Draw
+    </button>
+  ) : (
+    <Fragment />
+  );
 };
 
 export default DrawCardButton;
